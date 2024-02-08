d79efaf999e84f9ddebb329934129eef1a3c5e15
Marianne Anderson <mganderson13@gmail.com>
2024-02-08 14:19:31 -0600

    saved navbar file

--- Book-Branch/src/components/Navbar.jsx
+++ Book-Branch/src/components/Navbar.jsx
@@ -15,6 +15,9 @@ function Navbar() {
         <Link to="/search">Search</Link>
       </li>
       <li>
+        <Link to="/login">Login</Link>
+      </li>
+      <li>
         <Link to="/register">Register</Link>
       </li>
         </ul>
